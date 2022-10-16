22c07089116a605efa586442f78740679903c3f3
devzolo <zolotorevsky@gmail.com>
2022-10-16 06:20:32 -0300

    Updates

---
+++ examples/gui/src/framework/Renderer2D.ts
@@ -0,0 +1,22 @@
+import gl from "@devzolo/node-native-gl";
+import glut from "@devzolo/node-native-glut";
+import glm from "@devzolo/node-native-glm";
+import { Camera } from "utils";
+
+export class Renderer2D {
+  static beginScene(camera: Camera, transform: glm.mat4): void {
+
+  }
+
+  static endScene(): void {
+
+  }
+
+  static flush(): void {
+
+  }
+
+  static drawQuad(position: glm.vec2, size: glm.vec2, color: glm.vec4): void {
+
+  }
+}
